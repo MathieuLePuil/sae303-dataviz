6df04551799cd8976c41ad4d31d9ad51f1be2d03
Evan <94895335+WarLay0@users.noreply.github.com>
2022-10-21 10:09:53 +0200

    :heavy_plus_sign: | Création /regcom.js/ et ajout de jquery

--- index.php
+++ index.php
@@ -5,8 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SAE 303 | Dataviz</title>
+    <script src="https://code.jquery.com/jquery-3.6.1.js"></script>
+    <script src="js/regcom.js" type="module"></script>
 </head>
 <body>
-    
+    <main>
+        <div class="graph">
+            <canvas id=""></canvas>
+        </div>
+    </main>
 </body>
 </html>
