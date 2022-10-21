3747bd92e775c324bbf3e76858e383ebfdb0a40c
Evan <94895335+WarLay0@users.noreply.github.com>
2022-10-21 10:39:04 +0200

    :chart_with_upwards_trend: | Création du premier graphique /regCom/ (valeurs aléatoires pour le moment)

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SAE 303 | Dataviz</title>
     <script src="https://code.jquery.com/jquery-3.6.1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="js/regcom.js" type="module"></script>
 </head>
 <body>
     <main>
         <div class="graph">
-            <canvas id=""></canvas>
+            <canvas id="regCom"></canvas>
         </div>
     </main>
 </body>
--- js/regcom.js
+++ js/regcom.js
@@ -1,3 +1,37 @@
+import dataJson from '../json/reg.json' assert { type: 'json' }
 $(document).ready(function(){
-    
+    const regCom = new Chart(
+        document.getElementById('regCom'),
+        {
+            type : 'line',
+            data:{
+                labels: [
+                    'Janvier',
+                    'Février',
+                    'Mars',
+                    'Avril',
+                    'Mai',               
+                    'Juin',             
+                    'Juillet',
+                    'Août',
+                    'Septembre',
+                    'Octembre',
+                    'Novembre',
+                    'Décembre'
+                ],
+                datasets: [{
+                    label: 'Reg',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: dataJson.datasets1,
+                  },
+                  {
+                    label: 'Com',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 50, 240)',
+                    data: dataJson.datasets2,
+                  }]
+            }
+        }
+    )
 })
