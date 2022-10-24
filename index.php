71a505e2d317724cc7e54e0abf7483c1635ca9a6
MathieuLePuil <mathieu.le-puil@etudiant.univ-reims.fr>
2022-10-24 14:56:12 +0200

    :chart_with_upwards_trend: | Ajout du premier graphique sans couleur

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+require 'start_html.inc.php';
+?>
+
+    <body>
+<?php
+    require 'nav_html.inc.php';
+?>
+
+    <section class="accueil" id="accueil">
+        <img src="assets/LOGO%20Muxe.svg" alt="Logo Muxe">
+        <h1>Muxe</h1>
+    </section>
+
+    <section class="nbcommande" id="nbcommande">
+        <div class="titre-section">
+            <h2>Nombre de commande</h2>
+        </div>
+        <div class="graphique">
+            <div id="graph1">
+                <canvas id="com"></canvas>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam delectus deserunt dolore dolorum enim eum exercitationem harum incidunt ipsa itaque, magnam maiores numquam odit similique tempora, veritatis, vero voluptatum?</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="client" id="client">
+        <div class="titre-section">
+            <h2>Informations client</h2>
+        </div>
+        <div class="graphique">
+            <div id="graph2">
+                <img src="assets/LOGO%20Muxe.svg" alt="graphique">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam delectus deserunt dolore dolorum enim eum exercitationem harum incidunt ipsa itaque, magnam maiores numquam odit similique tempora, veritatis, vero voluptatum?</p>
+            </div>
+            <div id="graph3">
+                <img src="assets/LOGO%20Muxe.svg" alt="graphique">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam delectus deserunt dolore dolorum enim eum exercitationem harum incidunt ipsa itaque, magnam maiores numquam odit similique tempora, veritatis, vero voluptatum?</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="categorie" id="categorie">
+        <div class="titre-section">
+            <h2>Catégories</h2>
+        </div>
+        <div class="graphique">
+            <div id="graph4">
+                <img src="assets/LOGO%20Muxe.svg" id="graph3" alt="graphique">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam delectus deserunt dolore dolorum enim eum exercitationem harum incidunt ipsa itaque, magnam maiores numquam odit similique tempora, veritatis, vero voluptatum?</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="commande" id="commande">
+        <div class="titre-section">
+            <h2>Informations commandes</h2>
+        </div>
+        <div class="graphique">
+            <div id="graph5">
+                <img src="assets/LOGO%20Muxe.svg" alt="graphique">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam delectus deserunt dolore dolorum enim eum exercitationem harum incidunt ipsa itaque, magnam maiores numquam odit similique tempora, veritatis, vero voluptatum?</p>
+            </div>
+            <div id="graph6">
+                <img src="assets/LOGO%20Muxe.svg" alt="graphique">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam delectus deserunt dolore dolorum enim eum exercitationem harum incidunt ipsa itaque, magnam maiores numquam odit similique tempora, veritatis, vero voluptatum?</p>
+            </div>
+        </div>
+        <div class="graphique2">
+            <div id="graph7">
+                <img src="assets/LOGO%20Muxe.svg" alt="graphique">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam delectus deserunt dolore dolorum enim eum exercitationem harum incidunt ipsa itaque, magnam maiores numquam odit similique tempora, veritatis, vero voluptatum?</p>
+            </div>
+            <div id="graph8">
+                <img src="assets/LOGO%20Muxe.svg" alt="graphique">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam delectus deserunt dolore dolorum enim eum exercitationem harum incidunt ipsa itaque, magnam maiores numquam odit similique tempora, veritatis, vero voluptatum?</p>
+            </div>
+        </div>
+    </section>
+    </body>
+
+<?php
+require 'end_html.inc.php';
+?>
--- start_html.inc.php
+++ start_html.inc.php
@@ -8,4 +8,12 @@
     <title>Muxe | Dataviz</title>
     <link rel="stylesheet" href="style/style.css">
     <link rel="icon" href="assets/LOGO%20Muxe.svg">
+
+    <script src="https://code.jquery.com/jquery-3.6.1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="js/com.js" type="module"></script>
+    <script src="js/comReg.js" type="module"></script>
+    <script src="js/comCat.js" type="module"></script>
+    <script src="js/monReg.js" type="module"></script>
+    <script src="js/monCat.js" type="module"></script>
 </head>
